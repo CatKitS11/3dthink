e244932b08855a814992491b0437daedcf47345e
Patchara <sombrousnorn@gmail.com>
2026-03-09 00:51:14 +0700

    feat(database): add role field in user table

--- prisma.config.ts
+++ prisma.config.ts
@@ -3,12 +3,15 @@
 import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
+const isMigrate = process.argv.includes("migrate") || process.argv.includes("push")
+
 export default defineConfig({
   schema: "prisma/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    // url: process.env["DATABASE_URL"],
+    url: isMigrate ? process.env["DIRECT_URL"] : process.env["DATABASE_URL"],
   },
 });
