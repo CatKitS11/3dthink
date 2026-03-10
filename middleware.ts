dd0554762da77bc0d387be3b0928de56a2c62fc4
Patchara <sombrousnorn@gmail.com>
2026-03-10 13:43:45 +0700

    refactor(auth): fix middleware

--- middleware.ts
+++ middleware.ts
@@ -4,14 +4,27 @@ import type { NextRequest } from "next/server";
 export async function middleware(req: NextRequest) {
   const sessionToken = req.cookies.get("better-auth.session_token");
 
-  if (!sessionToken && req.nextUrl.pathname.startsWith("/test3d")) {
+  if (!sessionToken) {
+    const loginUrl = new URL("/sign-in", req.url);
+    loginUrl.searchParams.set("callbackUrl", req.nextUrl.pathname);
     
-    return NextResponse.redirect(new URL("/sign-in", req.url));
+    return NextResponse.redirect(loginUrl);
   }
 
   return NextResponse.next();
 }
+// #### optional: protect specific routes ####
+// const protectedRoutes = ["/dashboard", "/editor", "/admin"];
+
+// const isProtected = protectedRoutes.some((route) =>
+//   req.nextUrl.pathname.startsWith(route)
+// );
+
+// if (isProtected && !sessionToken) {
+//   return NextResponse.redirect(new URL("/sign-in", req.url));
+// }
 
 export const config = {
-  matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+  matcher: ["/dashboard/:path*"],
+  // ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
 };
