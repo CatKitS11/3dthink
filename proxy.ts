faeb0d99196c48c43aa471cf45b60dc2455b5536
Patchara <sombrousnorn@gmail.com>
2026-03-10 20:03:35 +0700

    refactor(auth): fix middleware to proxy

--- proxy.ts
+++ proxy.ts
@@ -1,7 +1,7 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 
-export async function middleware(req: NextRequest) {
+export async function proxy(req: NextRequest) {
   const sessionToken = req.cookies.get("better-auth.session_token");
 
   if (!sessionToken) {
